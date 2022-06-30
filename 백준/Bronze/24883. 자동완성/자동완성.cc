7db0104c1e7d1140c0ad561f2e4294f6110ddf87
김동현 <55612222+lewns2@users.noreply.github.com>
2022-06-30 13:27:46 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define INFL 1e18
+#define INF 1e9
+
+
+int main() {
+    ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+
+    char c; cin >> c;
+    if(c == 'N' || c == 'n') cout << "Naver D2";
+    else cout << "Naver Whale";
+    
+}
