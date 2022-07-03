9b2c7667285bd1006993bbee3613b86572eea84d
김동현 <55612222+lewns2@users.noreply.github.com>
2022-07-03 16:31:31 +0900

    [Platinum V] Title: 조별과제 멈춰!, Time: 60 ms, Memory: 10852 KB -BaekjoonHub

---
+++ 백준/Platinum/23034. 조별과제 멈춰！/README.md
@@ -0,0 +1,12 @@
+# [Platinum V] 조별과제 멈춰! - 23034 
+
+[문제 링크](https://www.acmicpc.net/problem/23034) 
+
+### 성능 요약
+
+메모리: 10852 KB, 시간: 60 ms
+
+### 분류
+
+너비 우선 탐색(bfs), 그래프 이론(graphs), 그래프 탐색(graph_traversal), 최소 스패닝 트리(mst), 트리(trees)
+
---
+++ 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.cc
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define INFL 1e18
+#define INF 1e9
+
+const int mxN = 1e3+20;
+int n, m;
+vector<pair<int, pair<int, int>>> arr;
+vector<pair<int, int>> adj[mxN];
+int par[mxN], dist[mxN][mxN];
+
+
+int find(int x) {
+    if(par[x] == x) return x;
+
+    return par[x] = find(par[x]);
+}
+
+bool merge(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if(a == b) return false;
+    if(a < b) swap(a, b);
+    par[a] = b;
+    return true;
+}
+
+void dfs(int start, int v, int p = -1) {
+    for(auto nxt : adj[v]) {
+        int next = nxt.first;
+        int nextcost = nxt.second;
+        if(next != p) {
+            dist[start][next] = max({dist[start][next], dist[start][v], nextcost});
+            dfs(start, next, v);
+        }
+    }
+}
+
+int main() {
+    ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+    for(int i=1; i<=n; i++) {
+        par[i] = i;
+    }
+    for(int i=0; i<m; i++) {
+        int fr, to, val; cin >> fr >> to >> val;
+        arr.push_back({val, {fr, to}});
+        arr.push_back({val, {to, fr}});
+    }
+    sort(arr.begin(), arr.end());
+
+    int ans = 0;
+    for(int i=0; i<arr.size(); i++) {
+        int cost = arr[i].first;
+        int a = arr[i].second.first;
+        int b = arr[i].second.second;
+        
+        if(merge(a, b)) {
+            adj[a].push_back({b, cost});
+            adj[b].push_back({a, cost});
+            ans += cost;
+        }
+    }
+    
+    for(int i=1; i<=n; i++) {
+        dfs(i, i);
+    }
+
+    int q; cin >> q;
+    while(q--) {
+        int x, y; cin >> x >> y;
+        cout << ans - dist[x][y] << "\n";
+    }
+}
