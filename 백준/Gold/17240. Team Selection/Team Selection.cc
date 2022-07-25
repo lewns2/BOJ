6a6a348f3722ba6699d3a757d3af98aed885b27a
김동현 <55612222+lewns2@users.noreply.github.com>
2022-07-26 00:08:10 +0900

    [Gold I] Title: Team Selection, Time: 16 ms, Memory: 7492 KB -BaekjoonHub

--- 백준/Gold/17240. Team Selection/Team Selection.cc
+++ 백준/Gold/17240. Team Selection/Team Selection.cc
@@ -5,31 +5,33 @@ using namespace std;
 #define INFL 1e18
 #define INF 1e9
 
-ll ar[20005][5];
-ll dp[20005][1<<5];
+const int mxN = 20005;
+int n, ar[mxN][6];
+ll dp[mxN][1<<5];
 
 int main() {
     ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
+    cin >> n;
     for(int i=1; i<=n; i++) {
         for(int j=0; j<5; j++) {
             cin >> ar[i][j];
         }
     }
 
-
     ll ans = 0;
     for(int i=1; i<=n; i++) {
 
-        for(int j=0; j<(1<<j); j++) {
+        for(int j=0; j<(1<<5); j++) {
             dp[i][j] = dp[i-1][j];
         }
+
         for(int j=0; j<5; j++) {
-            for(int k=0; k<(1<<5); k++) {
+            for(int k=0; k<(1<<5); k++){
                 if(k & (1<<j)) continue;
                 dp[i][k|(1<<j)] = max(dp[i][k|(1<<j)], dp[i-1][k] + ar[i][j]);
             }
         }
+
         ans = max(ans, dp[i][(1<<5)-1]);
     }
     cout << ans;
