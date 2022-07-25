7814a1d1891776ded9956dda69b7826099905051
김동현 <55612222+lewns2@users.noreply.github.com>
2022-07-25 21:59:38 +0900

    [Gold I] Title: Team Selection, Time: 16 ms, Memory: 7804 KB -BaekjoonHub

--- 백준/Gold/17240. Team Selection/Team Selection.cc
+++ 백준/Gold/17240. Team Selection/Team Selection.cc
@@ -5,48 +5,34 @@ using namespace std;
 #define INFL 1e18
 #define INF 1e9
 
-const int mxN = 2e4+10;
-ll n, ar[20050][8];
-int dp[20050][8];
-int order[6], vis[6];
-ll ans = 0;
-
-void solve() {
-
-    ll cal[20050][8];
-    memset(cal, 0, sizeof(cal));
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=5; j++) {
-            int x = order[j-1];
-            cal[i][j] = max(cal[i-1][j], cal[i-1][j-1] + ar[i][x]);
-        }
-    }
-    ans = max(ans, cal[n][5]);
-}
-
-void recur(int cur = 0) {
-    if(cur == 5) {
-        solve();
-    }
-
-    for(int i=1; i<=5; i++) {
-        if(vis[i]) continue;
-        vis[i] = 1;
-        order[cur] = i;
-        recur(cur+1);
-        vis[i] = 0;
-    }
-}
+ll ar[20005][5];
+ll dp[20005][1<<5];
 
 int main() {
     ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
+    int n; cin >> n;
     for(int i=1; i<=n; i++) {
-        for(int j=1; j<=5; j++) {
+        for(int j=0; j<5; j++) {
             cin >> ar[i][j];
         }
     }
 
-    recur();
+
+    ll ans = 0;
+    for(int i=1; i<=n; i++) {
+
+        for(int j=0; j<(1<<j); j++) {
+            dp[i][j] = dp[i-1][j];
+        }
+        for(int j=0; j<5; j++) {
+            for(int k=0; k<(1<<5); k++) {
+                if(k & (1<<j)) continue;
+                dp[i][k|(1<<j)] = max(dp[i][k|(1<<j)], dp[i-1][k] + ar[i][j]);
+            }
+        }
+        ans = max(ans, dp[i][(1<<5)-1]);
+    }
     cout << ans;
-} 
+
+}
+
