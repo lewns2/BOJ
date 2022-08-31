b33e9056f65784bf7cfdc43dc5495859a455e093
김동현 <55612222+lewns2@users.noreply.github.com>
2022-08-31 17:05:30 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 7504 ms, Memory: 5192 KB -BaekjoonHub

---
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INFL 1e18
+#define INF 1e9
+
+int main() {
+    ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        
+        map<ll, ll> mp;
+        ll cur = -1, num = -1;
+        for(int i=0; i<n; i++) {
+            ll a; cin >> a;
+            ++mp[a];
+            if(mp[a] > cur) {
+                cur = mp[a];
+                num = a;
+            }
+        }
+        if(cur > n/2) cout << num << "\n";
+        else cout << "SYJKGW" << "\n";
+    }
+}
