bffb6a9b892a85f56546f7d6395d5a949bf3d3a7
김동현 <55612222+lewns2@users.noreply.github.com>
2022-10-19 22:52:07 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,32 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
+#define ll long long
+#define INF 1e9
 
-const int mxN=100, mxK=1e5; 
-int n, x;
-int c[mxN+1], dp[mxK+1];
-
+int n, k;
+int ar[101], dp[10001];
 int main() {
-	cin >> n >> x;
-	for(int i=0; i<n; i++) {
-		cin >> c[i];
-	}
-	
-	for(int i=1; i<=x; i++) {
-		dp[i] = 1e9;
-	}
-	
-	for(int j=0; j<n; j++) {
-		for(int i=1; i<=x; i++) {
-			if(i>=c[j]) {
-				dp[i] = min(dp[i], dp[i-c[j]]+1);
-			}
-		}
-	}
-	if(dp[x]==1e9) {
-		cout << -1 << endl;
-	}
-	else {
-	cout << dp[x] << endl;	
-	}
-	
-}
+    cin >> n >> k;
+
+    for(int i=0; i<n; i++) {
+        cin >> ar[i];
+    }
+    for(int i=0; i<=k; i++) dp[i] = 1e9;
+    for(int i=0; i<n; i++) {
+        if(ar[i] > 10000) continue;
+        dp[ar[i]] = 1;
+    }
+    
+    for(int i=0; i<n; i++) {
+        for(int j=1; j<=k; j++) {
+            if(j - ar[i] >= 0) {
+                dp[j] = min(dp[j], dp[j-ar[i]] + 1);
+            }
+        }
+    }
+    if(dp[k] == 1e9) cout << -1;
+    else cout << dp[k];
+} 
