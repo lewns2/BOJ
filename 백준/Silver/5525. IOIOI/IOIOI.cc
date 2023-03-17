9e15f9e885cc686852f789aecbd3b682368ed7cc
김동현 <55612222+lewns2@users.noreply.github.com>
2023-03-17 20:09:00 +0900

    [Silver I] Title: IOIOI, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -5,50 +5,34 @@ using namespace std;
 #define INFL 1e18
 #define INF 1e9
 
-vector<int> get_pi(string s) {
-    int n = s.size();
-    vector<int> pi(n);
-    pi[0] = 0;
-    for(int i=1, j=0; i<n; i++) {
-        while(j && s[i] ^ s[j]) {
-            j = pi[j-1];
-        }
-        if(s[i] == s[j]) j++;
-        pi[i] = j;
-    }
-    return pi;
-}
-
-int kmp(string s, string p) {
-    int ans = 0;
-    int n = s.size(), m = p.size();
-    vector<int> pi = get_pi(p);
-
-    for(int i=0, j=0; i<n; i++) {
-        while(j && s[i] ^ p[j]) {
-            j = pi[j-1];
-        }
-        if(s[i] == p[j]) {
-            if(j == m - 1) {
-                ans++;
-                j = pi[j];
-            }
-            else j++;
-        }
-    }
-    return ans;
-}
 
 int main() {
     ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, m; cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
     string s; cin >> s;
-    string p = "";
-    for(int i=0; i<n*2+1; i++) {
-       if(i%2) p += 'O';
-       else p += 'I';
+
+    int len = n*2+1;
+
+    int pos = 0, ans = 0;
+
+    while(pos + (n*2) < m) {
+        
+        bool ok = true;
+        for(int i=pos; i<pos+len; i++) {
+            int x = i - pos;            
+            if(x % 2 && s[i] == 'I') {
+                ok = false;
+                break;
+            }    
+            else if(x % 2 == 0 && s[i] == 'O') {
+                ok = false;
+                break;
+            }
+        }
+        if(ok) ans++;
+        pos++;
     }
-
-    cout << kmp(s, p);
-
+    
+    cout << ans;
 } 
