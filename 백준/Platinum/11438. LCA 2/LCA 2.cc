694e9b62c5981000366ed9ff13aedce8e588833f
김동현 <55612222+lewns2@users.noreply.github.com>
2025-02-10 16:04:30 +0900

    [Platinum V] Title: LCA 2, Time: 92 ms, Memory: 21860 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.cc
+++ 백준/Platinum/11438. LCA 2/LCA 2.cc
@@ -1,77 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
 #define INFL 2e18
 #define INF 1e9
 
-const int mxN = 1e5 + 50;
-int n;
-vector<vector<int>> arr;
-bool vis[mxN];
-int depth[mxN]; 
-int dp[mxN][30];
+const int mxN = 1e5+10, mxM = 1e5, LOG = 20; // 1e5
+int n, m;
+vector<int> arr[mxN];
+int par[mxN][LOG];
+int depth[mxN];
 
-void dfs(int v, int d) {
-    vis[v] = true;
+void dfs(int v, int p , int d) {
+    par[v][0] = p;
     depth[v] = d;
 
     for(int i=0; i<arr[v].size(); i++) {
-        int next = arr[v][i];
-        if(!vis[next]) {
-            dp[next][0] = v;
-            dfs(next, d+1);
-        }
+        int nxt = arr[v][i];
+        if(nxt == par[v][0]) continue;
+        dfs(nxt, v, d+1);
     }
 }
 
-void init() {
-    for(int j=1; j<30; j++) {
+void make_table() {
+    for(int j=1; j<LOG; j++) {
         for(int i=1; i<=n; i++) {
-            dp[i][j] = dp[dp[i][j-1]][j-1];
+            par[i][j] = par[par[i][j-1]][j-1];
         }
     }
 }
 
-int lca(int a, int b) {
-    if(depth[a] < depth[b]) swap(a, b);
-    int diff = depth[a] - depth[b];
-
-    for(int i=0; diff; i++) {
-        if(diff & 1) {
-            a = dp[a][i];
-        }
-        diff >>= 1;
+int lca(int x, int y) {
+    if(depth[x] < depth[y]) swap(x, y);
+    int diff = depth[x] - depth[y];
+    for(int i=0; i<LOG; i++) {
+        if(diff & (1<<i)) {
+            x = par[x][i];
+        }    
     }
-    
-    if(a == b) return a;
 
-    for(int i=29; i>-1; i--) {
-        if(dp[a][i] != dp[b][i]) {
-            a = dp[a][i];
-            b = dp[b][i];
+    if(x == y) return x;
+
+    for(int i=LOG-1; i>=0; i--) {
+        if(par[x][i] != par[y][i]) {
+            x = par[x][i];
+            y = par[y][i];
         }
     }
-    return dp[a][0];
     
+    return par[x][0];
 }
 
 int main() {
     ios_base ::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    arr.resize(n+10);
     for(int i=0; i<n-1; i++) {
-        int fr, to; cin >> fr >> to;
-        arr[fr].push_back(to);
-        arr[to].push_back(fr);
-    }
-
-    dfs(1, 0);
-    init();
-    int m; cin >> m;
-    while(m--) {
         int a, b; cin >> a >> b;
-        cout << lca(a, b) << "\n";
-    }
-}
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }    
 
+    dfs(1, -1, 1); // root, par, depth
+
+    make_table();
+
+    cin >> m;
+    while(m--) {
+        int x, y; cin >> x >> y;
+        cout << lca(x, y) << "\n";
+    }
+
+ }
